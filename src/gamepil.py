f5db948deb5431441eff8784664b1abc10e90996
marcus kamps <marcus.kamps@gmail.com>
2011-10-21 03:19:28 -0400

    Moved options into draw method
    Draw method is now invoked from outside the class, and size and
    number
    of generations are specified at that time.
    
    Also, image files are now named after the starting pattern.

--- src/gamepil.py
+++ src/gamepil.py
@@ -6,69 +6,75 @@ import game
 
 class GamePIL:
 
-    def __init__(self, pattern, cellsize, path):
-        self.path = path
+    def __init__(self, pattern):
+        self.pattern = pattern
 
         # load starting pattern
         start = set()
         with open('{0}.txt'.format(pattern), 'r') as smap:
-            mwidth, mheight, offset = smap.readline().strip().split(',')
+            self.width, self.height, self.offset = [int(num) for num in smap.readline().strip().split(',')]
             for y, line in enumerate(smap.readlines()):
                 for x, value in enumerate(line):
                     if value == '.':
                         start.add((x, y))
         
-        # init image attributes
-        self.cellsize = cellsize
-        self.size = (int(mwidth) * self.cellsize, int(mheight) * self.cellsize)
-        self.origin = int(offset) * self.cellsize
-
         # init game simulator
         self.game = game.Game(start)
         self.cells = self.game.cells
 
-        # load cell sprite
+        # load and slice cell sprite
         sprite = Image.open('cell.png')
-        swidth, sheight = sprite.size
-        length = swidth / sheight
+        width, height = sprite.size
+        length = width / height
         self.animlength = length / 2
-
-        # resize and slice cell sprite
         self.sprites = []
-        if sheight != self.cellsize:
-            sprite = sprite.resize((self.cellsize * length, self.cellsize), Image.ANTIALIAS)
         for state in range(length):
-            self.sprites.append(sprite.crop((state * self.cellsize, 0, state * self.cellsize + self.cellsize, self.cellsize)))
+            self.sprites.append(sprite.crop((state * height, 0, (state + 1) * height, height)))
 
-        # start loop
-        for cycle in range(8):
-            self.draw_cycle(cycle)
+        
+    def draw(self, cycles, path, cellsize=None):
+        self.path = path
+        
+        # resize sprites if necessary
+        defaultsize = self.sprites[0].size[0]
+        if cellsize is None:
+            cellsize = defaultsize
+            
+        if cellsize != defaultsize:
+            for i, sprite in enumerate(self.sprites):
+                self.sprites[i] = sprite.resize((cellsize, cellsize), Image.ANTIALIAS if cellsize < defaultsize else Image.BILINEAR)
+            
+        # set image attributes
+        size = (self.width * cellsize, self.height * cellsize)
+        offset = self.offset * cellsize
 
+        # animate a single generation
+        for cycle in range(cycles):
+            newcells = self.game.next_generation()
+            birthing = newcells - self.cells
+            dying = self.cells - newcells
+            
+            self.image = Image.new('RGBA', size, (0, 0, 0, 0))
+            for state in range(self.animlength):
+                if state == 0:
+                    for x, y in self.cells:
+                        self.image.paste(self.sprites[self.animlength], (x * cellsize + offset, y * cellsize + offset))
+                else:
+                    for x, y in birthing:
+                        self.image.paste(self.sprites[state], (x * cellsize + offset, y * cellsize + offset))
+                    for x, y in dying:
+                        self.image.paste(self.sprites[state + self.animlength], (x * cellsize + offset, y * cellsize + offset))
+    
+                self.image.save(os.path.join(self.path, '{0}{1:03}.png'.format(self.pattern, cycle * self.animlength + state)))
+                
+            self.cells = newcells
+            
         print 'done.'
-            
-        
-    def draw_cycle(self, cycle):
-        newcells = self.game.next_generation()
-        birthing = newcells - self.cells
-        dying = self.cells - newcells
-        
-        self.image = Image.new('RGBA', self.size, (0, 0, 0, 0))
-        for state in range(self.animlength):
-            if state == 0:
-                for x, y in self.cells:
-                    self.image.paste(self.sprites[self.animlength], (x * self.cellsize + self.origin, y * self.cellsize + self.origin))
-            else:    
-                for x, y in birthing:
-                    self.image.paste(self.sprites[state], (x * self.cellsize + self.origin, y * self.cellsize + self.origin))
-                for x, y in dying:
-                    self.image.paste(self.sprites[state + self.animlength], (x * self.cellsize + self.origin, y * self.cellsize + self.origin))
-
-            self.image.save(os.path.join(self.path, 'frame{0:03}.png'.format(cycle * self.animlength + state)))
-            
-        self.cells = newcells
 
                 
                 
 if __name__ == '__main__':
-    GamePIL('galaxy', 50, 'd:\projects\gameoflife')
+    galaxy = GamePIL('galaxy')
+    galaxy.draw(8, 'd:\projects\gameoflife', 50)
+    
             
